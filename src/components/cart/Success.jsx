fa0ab1d0d474e47888ca910da7b1ceaaf87e72a3
Mohammed Rafik <mohammed.rafik@tvs.in>
2025-12-14 20:20:04 +0530

    integrete

--- src/components/cart/Success.jsx
+++ src/components/cart/Success.jsx
@@ -2,29 +2,28 @@ import React from "react";
 import "../../styles/cart/Success.css";
 
 const Success = ({ loading, showSuccess }) => {
+  if (!loading && !showSuccess) return null;
+
   return (
     <>
+      {/* Overlay */}
+      <div className="overlay"></div>
+
       {/* Loader */}
       {loading && (
-        <>
-          <div className="overlay"></div>
-          <div className="loading-center">
-            <div className="loader"></div>
-          </div>
-        </>
+        <div className="loading-center">
+          <div className="loader"></div>
+        </div>
       )}
 
       {/* Success Popup */}
       {showSuccess && (
-        <>
-          <div className="overlay"></div>
-          <div className="success-popup-center">
-            <div className="success-card">
-              <div className="success-tick">✔</div>
-              <p className="success-msg">Thank you for your Order!</p>
-            </div>
+        <div className="success-popup-center">
+          <div className="success-card">
+            <div className="success-tick">✔</div>
+            <p className="success-msg">Thank you for your Order!</p>
           </div>
-        </>
+        </div>
       )}
     </>
   );
